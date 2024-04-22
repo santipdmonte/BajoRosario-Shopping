cd609f82bb4c2ce8e6fdaef7d48c3cde87caedb7
santipdmonte <santiagopedemonte02@gmail.com>
2024-04-22 15:52:37 -0300

    cambio a rutas relativas los path de logo.png y .css

--- views/header.php
+++ views/header.php
@@ -11,7 +11,7 @@
     <!-- Boostrap CSS -->    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
-    <link rel="stylesheet" href="styles/index.css"> 
+    <link rel="stylesheet" href="/bajorosario-shopping/styles/index.css"> 
   </head>
 
 
@@ -26,7 +26,7 @@
   <nav class="navbar navbar-expand-lg shadow">
   <div class="container-fluid">
     <a href="/bajorosario-shopping/">
-      <img src="/assets/img/logo.png" alt="Bajo Rosario Logo" style="width: 50px;">
+      <img src="/bajorosario-shopping/assets/img/logo.png" alt="Bajo Rosario Logo" style="width: 50px;">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
