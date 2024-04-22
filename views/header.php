c1a30adc6466b7decc6bf397e6d43deea6609af2
santipdmonte <santiagopedemonte02@gmail.com>
2024-04-22 10:54:21 -0300

    Update header logo in header.php

--- views/header.php
+++ views/header.php
@@ -24,7 +24,9 @@
 
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/bajorosario-shopping/">Bajito Rosario</a>
+    <a href="/bajorosario-shopping/">
+      <img src="assets/img/logo.png" alt="Bajo Rosario Logo" style="width: 50px;">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
