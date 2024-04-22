72c48fa9c770be291af35ac3b7c1a27e55f170ef
santipdmonte <santiagopedemonte02@gmail.com>
2024-04-22 10:51:51 -0300

    Add favicon to header.php

--- views/header.php
+++ views/header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bajito Rosario | Promociones</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/logo.ico">
 
     
     <!-- Boostrap CSS -->    
