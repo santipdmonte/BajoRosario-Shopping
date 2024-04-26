abfb711d036a6717e6072ef259324903be5e500c
santipdmonte <santiagopedemonte02@gmail.com>
2024-04-26 20:30:20 -0300

    new 404 page

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php include("src/views/header.php")?>
+
+<section class="container">
+    <h1 class="title">Error 404 - Página no encontrada</h1>
+    <div class="p-4">
+        <p>Lo sentimos, la página que estás buscando no existe.</p>
+        <a href="/bajorosario-shopping/">
+            <button class="btn btn-primary">
+                Pagina Principal
+            </button>
+        </a>
+    </div>
+</section>
+
+    
+<?php include("src/views/footer.html")?>
