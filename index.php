9393acf37398cadff41460cda521432ae19b9eb0
santipdmonte <santiagopedemonte02@gmail.com>
2024-04-30 19:27:28 -0300

    Nuevo fondo para index (cambiar a index.css)

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
 <?php  include ('src/views/hero_section.html')?>
 
+<style>
+    body {
+  background: #f2f2f2;
+}
+</style>
+
 <!-- TODO: Carusel de Marcas -->
 <section class="text-center p-5" >
     <h3 class="bold text-xl fs-1" style="text-shadow: 0.5px 0.5px 1px rgba(0, 0, 0, 0.5);">180+ TIENDAS</h3>
