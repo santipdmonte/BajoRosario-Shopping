70ee3e137cf5368ecb97fca2285f4c0df2f15c82
santipdmonte <santiagopedemonte02@gmail.com>
2024-06-16 11:10:26 -0300

    Nueva ruta 'styles/index.css' -> '/bajorosario-shopping/' Solucion de problemas vistas en carpetas internas

--- src/views/header.php
+++ src/views/header.php
@@ -13,7 +13,7 @@
     <!-- Boostrap CSS -->    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
-    <link rel="stylesheet" href="styles/index.css"> 
+    <link rel="stylesheet" href="/bajorosario-shopping/styles/index.css"> 
   </head>
 
 
