eba65ac7d97c859615c729becd2d4c12d43b77a0
Francisco <Franciscovelazquezz2002@gmail.com>
2024-06-18 17:29:08 -0300

    Validacion de fecha de novedades

--- src/views/novedades.php
+++ src/views/novedades.php
@@ -3,6 +3,7 @@ include __DIR__ . "/header.php";
 ?>
 
 <!-- TODO: Mostar solo las promo a la fecha y validar el tipo de cliente -->
+ <!-- chequear la columna de estado para novedades, no tiene sentido si valida por fecha-->
 
 <section class="section">
     <h2 class="title"> Novedades </h2>
@@ -33,17 +34,25 @@ include __DIR__ . "/header.php";
         <?php
         include "../controllers/novedades/get_novedades.php";
         $result = get_novedades_by_user();
+
         
-        while($row = mysqli_fetch_array($result)){ ?>
-            
-            <!-- Itero por cada fila de promociones en la DB -->
-            <div class="card" style="width: 18rem;">
-                <div href="#" class="card-body">
-                    <h5 class="card-title"><?php echo $row['texto_novedad']?></h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">Hasta el <?php echo $row['fecha_hasta_novedad']?></h6>
+        if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_array($result)) { ?>
+                <!-- Itero por cada fila de promociones en la DB -->
+                <div class="card" style="width: 18rem;">
+                    <div href="#" class="card-body">
+                        <h5 class="card-title"><?php echo $row['texto_novedad']?></h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Hasta el <?php echo $row['fecha_hasta_novedad']?></h6>
+                    </div>
                 </div>
+            <?php }
+        } else { ?>
+            <!-- Mostrar mensaje si no hay novedades -->
+            <div class="alert alert-danger" role="alert" style="font-weight: bold; width: 70%; text-align: center;font-size: 1.5em; font-color:black;">
+                No hay novedades por hoy.
             </div>
-        <?php }?>
+        <?php } ?>
+
     
     
     </div>
