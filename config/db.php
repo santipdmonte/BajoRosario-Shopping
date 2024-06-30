80ccb8db2b1ffd13c0eee1d315a88da2b764c521
MateoGaraventa <mateoogaraventa@gmail.com>
2024-06-30 10:46:53 -0300

    Se realiza la edicion de las novedades

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 $server = 'localhost';
 $username = 'root';
-$password = '';
+$password = 'maateo123';
 $dbname = 'bajito_rosario_promociones';
 
 $conn = mysqli_connect($server, $username, $password, $dbname);
