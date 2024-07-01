aea1c4a92efacea5aa27a9a9cbe7c4acb6791a0e
Francisco <Franciscovelazquezz2002@gmail.com>
2024-07-01 11:05:26 -0300

    validacion del formulario nueva promo

--- src/controllers/promociones/promo_new.php
+++ src/controllers/promociones/promo_new.php
@@ -10,25 +10,40 @@ if (isset($_POST['save_promo'])){
     $categoria_cliente = $_POST['categoria_cliente'];
     $cod_local = $_POST['cod_local'];
 
-    // Recorrer el array de días seleccionados y activar los dias seleccionados
-    $dias_semana = [0,0,0,0,0,0,0];
-    foreach ($_POST['dias'] as $dia) {
-        $dias_semana[$dia] = 1;
-    }
-    $dias_semana = json_encode($dias_semana); // Convertir el array a string
+    $query = "SELECT * FROM locales WHERE cod_local = '$cod_local'";
+    $local = mysqli_query($conn, $query);
 
-    $result = save_promo($conn, $texto_promo, $fecha_desde_promo, $fecha_hasta_promo, $categoria_cliente, $dias_semana, $cod_local);
+    // Validar que las fechas sean válidas y el local exista
+    if ($local->num_rows != 0){
+        if (!empty($fecha_desde_promo) && DateTime::createFromFormat('Y-m-d', $fecha_desde_promo) !== false){
+            if (!empty($fecha_hasta_promo) && DateTime::createFromFormat('Y-m-d', $fecha_hasta_promo) !== false) {
+                if ($fecha_desde_promo > $fecha_hasta_promo){
+                    echo "La fecha de inicio debe ser menor a la fecha de fin";
+                } else {
+                    $dias_semana = [0,0,0,0,0,0,0];
+                    foreach ($_POST['dias'] as $dia) {
+                        $dias_semana[$dia] = 1;
+                    }
+                    $dias_semana = json_encode($dias_semana); // Convertir el array a string
+
+                    $result = save_promo($conn, $texto_promo, $fecha_desde_promo, $fecha_hasta_promo, $categoria_cliente, $dias_semana, $cod_local);
+                }
+            } else{ echo "La fecha de fin no es válida"; }
+        } else{ echo "La fecha de inicio no es válida"; }
+    } else { echo "El local no existe"; }
+
+    // Recorrer el array de días seleccionados y activar los dias seleccionados
+    session_start();
 
     if (!$result){
         $_SESSION['promo_failed'] = true;
 
-        header("Location: /bajorosario-shopping/dueno/new_promo");
-        exit(); 
+    } else{
+        $_SESSION['promo_saved'] = true;
     }
 
     // Establecer variable de sesión para indicar que la promoción se ha guardado con éxito
-    session_start();
-    $_SESSION['promo_saved'] = true;
+   
 
     header("Location: /bajorosario-shopping/dueno/new_promo");
     exit();
