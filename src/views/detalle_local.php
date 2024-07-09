0562de7b1889b5b6d2f0d5e12ff9691b2ffd9638
santipdmonte <santiagopedemonte02@gmail.com>
2024-07-09 15:36:19 -0300

    new style, no definitive

--- src/views/detalle_local.php
+++ src/views/detalle_local.php
@@ -17,81 +17,92 @@
 
     <?php if ($result_local) { ?>
 
-        <div class="d-flex justify-content-center" style="width: 100%;">
-            <div style="width: 80%;">
+        <div 
+            class="container p-4" 
+            style="
+            display: flex; 
+            flex-wrap: wrap; 
+            gap: 10px; 
+            justify-content: center; 
+            align-items: center;">
+        
+            <div class="d-flex justify-content-center" style="width: 100%;">
+                <div style="width: 100%;">
 
-                <div class="text-center">
-                    <h1 style="font-family: Times New Roman;"><?php echo $result_local['nombre_local']?></h1>
+                    <div class="text-center">
+                        <h1 style="font-family: Times New Roman;"><?php echo $result_local['nombre_local']?></h1>
+                    </div>
+                    <hr/>
+
+                    <div class="text-left">
+                        <p class="fs-4"><strong>Ubicación:</strong> <?php echo $result_local['ubicacion_local']?></p>
+                    </div>
+
+                    <div class="text-left">
+                        <p class="fs-4"><strong>Rubro:</strong> <?php echo $result_local['rubro_local']?></p>
+                    </div>
+
+                    <div class="text-center">
+                        <img src="<?php echo $result_local['url_logo']?>" alt="Logo del local" style="max-width: 100%;">
+                    </div>
+
                 </div>
                 <hr/>
-
-                <div class="text-left">
-                    <p class="fs-4"><strong>Ubicación:</strong> <?php echo $result_local['ubicacion_local']?></p>
-                </div>
-
-                <div class="text-left">
-                    <p class="fs-4"><strong>Rubro:</strong> <?php echo $result_local['rubro_local']?></p>
-                </div>
-
-                <div class="text-center">
-                    <img src="<?php echo $result_local['url_logo']?>" alt="Logo del local" style="max-width: 100%;">
-                </div>
-
             </div>
-            <hr/>
-        </div>
 
 
-        <div class="w-80">
-            <h2 class="title">Promociones</h2>
-            <div 
-                class="container p-4" 
-                style="
-                    display: flex; 
-                    flex-wrap: wrap; 
-                    gap: 10px; 
-                    justify-content: center; 
-                    align-items: center;"
-                >
-                
-                <?php while($promo = mysqli_fetch_array($promociones_by_client_store)){ ?>
+            <div class="w-80">
+                <h2 class="title">Promociones</h2>
+                <div 
+                    class="container p-4" 
+                    style="
+                        display: flex; 
+                        flex-wrap: wrap; 
+                        gap: 10px; 
+                        justify-content: center; 
+                        align-items: center;"
+                    >
                     
-                    <!-- Itero por cada fila de promociones en la DB -->
-                    <div class="card" style="width: 18rem;">
-                        <div href="#" class="card-body">
+                    <?php while($promo = mysqli_fetch_array($promociones_by_client_store)){ ?>
+                        
+                        <!-- Itero por cada fila de promociones en la DB -->
+                        <div class="card" style="width: 18rem;">
+                            <div href="#" class="card-body">
 
-                            <div style="display: flex; justify-content: space-between;">
-                                <h5 class="card-title"><?php echo $promo['texto_promo']?></h5>
-                                <div>
-                                    <span class="badge text-bg-info"><?php echo $promo['categoria_cliente']?></span>
+                                <div style="display: flex; justify-content: space-between;">
+                                    <h5 class="card-title"><?php echo $promo['texto_promo']?></h5>
+                                    <div>
+                                        <span class="badge text-bg-info"><?php echo $promo['categoria_cliente']?></span>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <h6 class="card-subtitle mb-2 text-body-secondary">
-                                Hasta el <?php echo $promo['fecha_hasta_promo']?>
-                            </h6>
+                                <h6 class="card-subtitle mb-2 text-body-secondary">
+                                    Hasta el <?php echo $promo['fecha_hasta_promo']?>
+                                </h6>
 
-                            <?php if (isset($_SESSION['user'])){?>
-                                <form action="/bajorosario-shopping/src/controllers/promociones/promo_uso.php" method="POST">
-                                    <input type="text" name="cod_promo" value="<?php echo $promo['cod_promo']?>" hidden>
-                                    <input type="text" name="cod_usuario" value="<?php echo $_SESSION['cod_usuario']?>" hidden>
-                                    <?php 
-                                    echo validate_category_access($conn, $promo) ? 
-                                        '<button type="submit" class="btn btn-primary w-100" name="use_promo">Obtener Promoción</button>' : 
-                                        '<button type="submit" class="btn btn-secondary w-100 disabled" name="use_promo">Obtener Promoción</button>';  
-                                    ?>
-                                </form>
-                            <?php } else {?>
-                                <form action="/bajorosario-shopping/inicio_sesion" method="POST">
-                                    <button type="submit" class="btn btn-secondary w-100" name="use_promo_no_sesion">Obtener Promoción</button>
-                                </form>
-                            <?php } ?>
+                                <?php if (isset($_SESSION['user'])){?>
+                                    <form action="/bajorosario-shopping/src/controllers/promociones/promo_uso.php" method="POST">
+                                        <input type="text" name="cod_promo" value="<?php echo $promo['cod_promo']?>" hidden>
+                                        <input type="text" name="cod_usuario" value="<?php echo $_SESSION['cod_usuario']?>" hidden>
+                                        <?php 
+                                        echo validate_category_access($conn, $promo) ? 
+                                            '<button type="submit" class="btn btn-primary w-100" name="use_promo">Obtener Promoción</button>' : 
+                                            '<button type="submit" class="btn btn-secondary w-100 disabled" name="use_promo">Obtener Promoción</button>';  
+                                        ?>
+                                    </form>
+                                <?php } else {?>
+                                    <form action="/bajorosario-shopping/inicio_sesion" method="POST">
+                                        <button type="submit" class="btn btn-secondary w-100" name="use_promo_no_sesion">Obtener Promoción</button>
+                                    </form>
+                                <?php } ?>
 
-                        </div> 
-                    </div>
-                <?php } ?>
+                            </div> 
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
         </div>
+
 
     <?php } else {?>
 
