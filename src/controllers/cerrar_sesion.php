a73b0c309027d241c1b869ca6fbe9d825b7aa4e9
santipdmonte <santiagopedemonte02@gmail.com>
2024-07-11 17:26:07 -0300

    Aplicamos buenas practicas para cerrar sesion

--- src/controllers/cerrar_sesion.php
+++ src/controllers/cerrar_sesion.php
@@ -2,6 +2,12 @@
 
 session_start();
 session_destroy();
+$_SESSION['login']=false;
+    $_SESSION['cod_usuario']='';
+    $_SESSION['nombre_usuario']='';
+    $_SESSION['user']='';
+    $_SESSION['categoria_cliente']='';
+
 header('Location: /bajorosario-shopping/')
 
 ?>
